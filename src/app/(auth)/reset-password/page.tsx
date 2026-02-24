fd2b94c7f34bae97799f1bb63d0f3272cad35e92
Amoly93 <amalbajber93@gmail.com>
2026-02-24 11:47:58 +0300

    fix

--- src/app/(auth)/reset-password/page.tsx
+++
@@ -1,5 +0,0 @@
-import ResetPasswordScreen from "@/components/screens/auth/password-screen/components/reset-password/reset-password-screen";
-
-export default function ResetPasswordPage() {
-  return <ResetPasswordScreen />;
-}
